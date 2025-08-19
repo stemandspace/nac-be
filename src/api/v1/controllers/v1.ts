2844a2e1afa88a204761206ec461dd98096282e1
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2025-08-19 23:11:00 +0530

    Refactor payment processing condition in webhook handler to ensure only payments with 'NAC25' in the description are processed for student retrieval.

--- src/api/v1/controllers/v1.ts
+++ src/api/v1/controllers/v1.ts
@@ -163,7 +163,7 @@ export default factories.createCoreController('api::v1.v1', ({ strapi }) => ({
             console.log("body", JSON.stringify(body, null, 2));
             const payment = body.payload.payment.entity;
 
-            if (body.event === 'payment.captured' && !payment.description || !payment.description.includes('NAC25')) {
+            if (body.event === 'payment.captured' && payment.description.includes('NAC25')) {
                 // Find student by order ID
                 const student = await strapi.documents('api::student.student').findOne({
                     documentId: payment.notes.student_document_id
